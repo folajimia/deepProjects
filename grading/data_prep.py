5b1ad67a55bead33b996bfac973b9d1e05a0d03f
Jimi Adekoya <jimi.adekoya@mheducation.com>
2018-07-26 22:48:57 +0200

    first project grade predictor

---
+++ grading/data_prep.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+
+admissions = pd.read_csv('binary.csv')
+
+# Make dummy variables for rank
+# how dummy variables work -https://towardsdatascience.com/the-dummys-guide-to-creating-dummy-variables-f21faddb1d40
+
+data = pd.concat([admissions, pd.get_dummies(admissions['rank'], prefix='rank')], axis=1)
+data = data.drop('rank', axis=1)
+
+
+# Standardize features
+
+for field in ['gre', 'gpa']:
+    mean, std = data[field].mean(),data[field].std()
+    data.loc[:,field] = (data[field]- mean)/std
+
+#split of random 10% of data for testing
+
+np.random.seed(42)
+sample = np.random.choice(data.index, size=int(len(data)*0.9), replace=False)
+data, test_data = data.ix[sample], data.drop(sample)
+
+
+#split into features and targets
+
+features, targets = data.drop('admit', axis=1), data['admit']
+features_test, targets_test = test_data.drop('admit' , axis=1), test_data['admit']
+
