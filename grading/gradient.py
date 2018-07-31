ce380a77081e7534804061b53f3ea941c6b4fa2e
Jimi Adekoya <jimi.adekoya@mheducation.com>
2018-07-31 05:08:28 +0200

    modified to include a hidden layer for multilayer perceptron and adding backpropargation

--- grading/gradient.py
+++ grading/gradient.py
@@ -22,31 +22,54 @@ last_loss = None
 # 1/n^0.5 where n is the number of input units. This keeps the input to the sigmoid low for increasing
 # numbers of input units.
 
-weights = np.random.normal(scale=1 / n_features**.5, size=n_features)
+#weights = np.random.normal(scale=1 / n_features**.5, size=n_features)
+
+weights_input_hidden = np.random.normal(scale=1 / n_features**.5, size=(n_features, n_hidden))
+weights_hidden_output = np.random.normal(scale=1/ n_features**.5, size=n_hidden)
 
 
 # Neural network hyper parameters
+n_hidden = 2
 epochs = 1000
 learnrate = 0.5
 
 
 
 for e in range(epochs):
-    del_w = np.zeros(weights.shape)
+    #del_w = np.zeros(weights.shape)
+    del_w_input_hidden = np.zeros(weights_input_hidden.shape)
+    del_w_hidden_output = np.zero(weights_hidden_output)
     #Loop through all records, x is the input, y is the target
     for x, y in zip(features.values, targets):
         #calculating the output
-        output = sigmoid(np.dot(x, weights))
+        #output = sigmoid(np.dot(x, weights))
+        hidden_output = sigmoid(np.dot(x, weights_input_hidden))
+        output = sigmoid(np.dot(hidden_output, weights_hidden_output))
 
         #calculate the error
         error =  y - output
 
+
+        #backprop error term for output unit is
+
+        BackErrorOutput = error * sigmoid_prime(x)
+
+
+        #backprop error term for hidden unit is
+
+        BackErrorHidden =  BackErrorOutput * (hidden_output*(1-hidden_output))
+
         #the gradient descent step, the error times the gradient times the inputs
         #del_w += error * output * (1-output) * x
-        del_w += error * sigmoid_prime(x) * x
+        #del_w += error * sigmoid_prime(x) * x
+        del_w_hidden_output += BackErrorOutput * hidden_output
+        del_w_input_hidden += BackErrorHidden * x[:, None]
 
     #Update weights
-    weights += learnrate * del_w / n_records
+    #weights += learnrate * del_w / n_records
+
+    weights_input_hidden = learnrate * del_w_input_hidden / n_records
+    weights_hidden_output = learnrate * del_w_hidden_output / n_records
 
     #Printing out the mean square error on the training set
     if e % (epochs/ 10)== 0:
@@ -61,7 +84,11 @@ for e in range(epochs):
 
 # Calculate accuracy on test data
 
-test_out = sigmoid(np.dot(features_test, weights))
+#test_out = sigmoid(np.dot(features_test, weights))
+
+
+hidden = sigmoid(np.dot(features_test,weights_input_hidden))
+out = sigmoid(np.dot(hidden, weights_hidden_output))
 predictions = test_out > 0.5
 accuracy = np.mean(predictions==targets_test)
 print("Prediction accuracy: {:.3f}". format(accuracy))
