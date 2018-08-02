b2a34c86b929c2999b3dda39668c9133fae2d9a7
Jimi Adekoya <jimi.adekoya@mheducation.com>
2018-08-03 00:20:20 +0200

    add dummy values

--- bikeshare/data_prep.py
+++ bikeshare/data_prep.py
@@ -11,3 +11,17 @@ rides = pd.read_csv(data_path)
 print(rides.head())
 
 
+#plot of number of bike riders over the first 10 days in the data set
+#rides[:24*10].plot(x='dteday', y='cnt')
+#plt.show()
+
+# To include some categorical variables like season, weather, month in my model, they will be  made binary dummy variables
+
+dummy_fields = ['season', 'weathersit', 'mnth', 'hr', 'weekday']
+for each in dummy_fields:
+    dummies = pd.get_dummies(rides[each], prefix=each, drop_first=False)
+    rides = pd.concat([rides, dummies], axis=1)
+
+fields_to_drop = ['instant', 'dteday', 'season', 'weathersit', 'weekday', 'atemp', 'mnth', 'workingday', 'hr']
+data = rides.drop(fields_to_drop, axis=1)
+print(data.head())
