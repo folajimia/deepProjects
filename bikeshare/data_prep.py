6ecd57ef4fad18a3d45e941ae346c9eb54bf99aa
Jimi Adekoya <jimi.adekoya@mheducation.com>
2018-08-06 14:28:09 +0200

    standardize with mean and std

--- bikeshare/data_prep.py
+++ bikeshare/data_prep.py
@@ -8,10 +8,11 @@ import matplotlib.pyplot as plt
 
 data_path = 'Bike-Sharing-Dataset/hour.csv'
 rides = pd.read_csv(data_path)
-print(rides.head())
+#print(rides.head())
 
 
 #plot of number of bike riders over the first 10 days in the data set
+# to see the graph below uncomment
 #rides[:24*10].plot(x='dteday', y='cnt')
 #plt.show()
 
@@ -24,4 +25,18 @@ for each in dummy_fields:
 
 fields_to_drop = ['instant', 'dteday', 'season', 'weathersit', 'weekday', 'atemp', 'mnth', 'workingday', 'hr']
 data = rides.drop(fields_to_drop, axis=1)
+#print(data.head())
+
+
+# scaling target variables
+
+quant_features = ['casual', 'registered', 'cnt', 'temp', 'hum', 'windspeed']
+# Store scaling in a dictionary so we can convert back later
+scaled_features ={}
+for each in quant_features:
+    mean, std = data[each].mean(), data[each].std()
+    scaled_features[each] = [mean, std]
+    data.loc[:, each] = (data[each] - mean)/ std
+
+
 print(data.head())
