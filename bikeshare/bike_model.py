02e4e8d1bbacc809522fe70b88beec6c128fc958
Jimi Adekoya <jimi.adekoya@mheducation.com>
2018-08-12 21:47:14 +0200

    add to build fn

--- bikeshare/bike_model.py
+++ bikeshare/bike_model.py
@@ -36,9 +36,32 @@ class NeuralNetwork(object):
 
         # hidden layers
 
-        hidden_inputs = np.dot(self.weights_input_to_hidden, inputs)
+        hidden_inputs = np.dot(self.weights_input_to_hidden, inputs) #input to hidden layer
 
-        hidden_outputs = np.dot(self.activation_function(hidden_inputs))
+        hidden_outputs = np.dot(self.activation_function(hidden_inputs)) #hidden layer out activation fn
+
+        #output layer
+        final_outputs = np.dot(self.weights_hidden_to_output, hidden_outputs) #signals into final output layer
+
+        final_outputs = final_inputs
+
+        ### implement the backward pass  ###
+
+        # Output error
+        # error = y-y' = targets - final_outputs
+        output_errors = (targets - final_outputs) * 1 # Output layer error is the difference between desired
+        # target and actual output since outer layer input equals output
+
+        #Back propergated error
+        hidden_errors = np.dot(self.weights_hidden_to_output.T, output_errors) #errors propergated to the hidden layer
+        hidden_grad = hidden_outputs * (1-hidden_outputs) #hidden layer gradient
+
+
+        #update weights
+        self.weights_hidden_to_output += self.lr * output_errors * hidden_outputs.T
+        self.weights_input_to_hidden += self.lr * hidden_errors * hidden_grad * inputs.T
+
+
 
 
 
